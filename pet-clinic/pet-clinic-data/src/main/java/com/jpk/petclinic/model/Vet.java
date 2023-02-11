e9de6247886651446ce61ae19729e29ccfdf704f
Praveen Kumar Jana <praveenkumarjana83@gmail.com>
2023-02-11 13:51:18 +0800

    added Speciality entities

--- pet-clinic/pet-clinic-data/src/main/java/com/jpk/petclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/jpk/petclinic/model/Vet.java
@@ -2,4 +2,13 @@ package com.jpk.petclinic.model;
 
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
